13487430aa4fdfcf258d04325ca112c82716b88a
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-08-08 14:47:32 -0300

    Add translation load transform callback, to add prefixes to the i18n keys
    Still testing if it is a nice idea or not

--- plugin/src/main/kotlin/net/perfectdreams/i18nhelper/plugin/I18nHelperPluginExtension.kt
+++ plugin/src/main/kotlin/net/perfectdreams/i18nhelper/plugin/I18nHelperPluginExtension.kt
@@ -1,8 +1,12 @@
 package net.perfectdreams.i18nhelper.plugin
 
 import org.gradle.api.provider.Property
+import java.io.File
 
 interface I18nHelperPluginExtension {
     val generatedPackage: Property<String>
     val languageSourceFolder: Property<String>
+    val translationLoadTransform: Property<LoadTransformCallback>
 }
+
+typealias LoadTransformCallback = (File, Map<String, Any>) -> (Map<String, Any>)
