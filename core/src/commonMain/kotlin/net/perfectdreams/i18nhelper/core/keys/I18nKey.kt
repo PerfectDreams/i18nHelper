4f248048dfba24c1bf28d81ed6817dc2569e75ff
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-08-09 10:30:47 -0300

    Make the i18n keys serializable

--- core/src/commonMain/kotlin/net/perfectdreams/i18nhelper/core/keys/I18nKey.kt
+++ core/src/commonMain/kotlin/net/perfectdreams/i18nhelper/core/keys/I18nKey.kt
@@ -1,3 +1,17 @@
 package net.perfectdreams.i18nhelper.core.keys
 
-open class I18nKey(val key: String)
+import kotlinx.serialization.Serializable
+
+/**
+ * I18nKey represents any internationalization translation key represented by the [key] argument.
+ *
+ * Because the class is [Serializable], it is possible to use it inside of serializable objects, allowing
+ * to share the translation key itself.
+ *
+ * @see StringI18nKey
+ * @see ListI18nKey
+ */
+@Serializable
+sealed class I18nKey {
+    abstract val key: String
+}
--- core/src/commonMain/kotlin/net/perfectdreams/i18nhelper/core/keys/StringI18nKey.kt
+++ core/src/commonMain/kotlin/net/perfectdreams/i18nhelper/core/keys/StringI18nKey.kt
@@ -1,3 +1,16 @@
 package net.perfectdreams.i18nhelper.core.keys
 
-class StringI18nKey(key: String) : I18nKey(key)
+import kotlinx.serialization.Serializable
+
+/**
+ * StringI18nKey is used to store a translation [key], used for type safe calls in [net.perfectdreams.i18nhelper.core.I18nContext].
+ *
+ * Because the class is [Serializable], it is possible to use it inside of serializable objects, allowing
+ * to share the translation key itself.
+ *
+ * @param key the translation string key
+ * @see ListI18nKey
+ * @see I18nKey
+ */
+@Serializable
+class StringI18nKey(override val key: String) : I18nKey()
